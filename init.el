906b6ca82336a50effff9fd5af291dfb7ea301cc
Nicolás Hock Isaza <nhocki@gmail.com>
2014-12-11 23:06:35 -0800

    Mark the theme as safe in the new computer

--- init.el
+++ init.el
@@ -11,7 +11,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-safe-themes (quote ("d63e19a84fef5fa0341fa68814200749408ad4a321b6d9f30efc117aeaf68a2e" default)))
+ '(custom-safe-themes
+   (quote
+    ("9dae95cdbed1505d45322ef8b5aa90ccb6cb59e0ff26fef0b8f411dfc416c552" "d63e19a84fef5fa0341fa68814200749408ad4a321b6d9f30efc117aeaf68a2e" default)))
  '(safe-local-variable-values (quote ((encoding . utf-8)))))
 
 (load "00common-setup")
@@ -25,3 +27,9 @@
 
 (require 'textmate)
 (textmate-mode)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
